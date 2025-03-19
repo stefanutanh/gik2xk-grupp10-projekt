6d5b66b836332835b33f3d13e8765e7990845a38
stefanutanh <bergs83@gmail.com>
2025-03-19 14:10:04 +0100

    Add files via upload

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const { where } = require("sequelize");
+const db = require('../models');
+
+const postService = require('../services/postService');
+
+
+router.get('/', (req, res) => {
+    postService.getAll().then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
+router.post('/', (req, res) => {
+    const post = req.body;
+    postService.create(post).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
+router.put('/', (req, res) => {
+    const post = req.body;
+    const id = post.id;
+    
+    postService.update(post, id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+});
+
+router.delete('/', (req, res) => {
+    const id = req.body.id;
+    postService.destroy(id).then((result) => {
+        res.status(result.status).json(result.data);
+    });
+    
+});
+
+module.exports = router;
